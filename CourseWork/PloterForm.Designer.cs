9b6727d3300ab9a57cc2815d0a93ef9f7d744dac
Gresnik22 <qq224260@gmail.com>
2022-11-11 09:06:06 +0100

    style: updated ui

--- CourseWork/PloterForm.Designer.cs
+++ CourseWork/PloterForm.Designer.cs
@@ -64,124 +64,124 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(222, 229);
+            this.groupBox1.Size = new System.Drawing.Size(272, 248);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Загальні дані";
             // 
             // tbPrice
             // 
-            this.tbPrice.Location = new System.Drawing.Point(109, 188);
+            this.tbPrice.Location = new System.Drawing.Point(166, 193);
             this.tbPrice.Name = "tbPrice";
-            this.tbPrice.Size = new System.Drawing.Size(100, 23);
+            this.tbPrice.Size = new System.Drawing.Size(100, 36);
             this.tbPrice.TabIndex = 11;
             // 
             // tbWeight
             // 
-            this.tbWeight.Location = new System.Drawing.Point(109, 158);
+            this.tbWeight.Location = new System.Drawing.Point(166, 163);
             this.tbWeight.Name = "tbWeight";
-            this.tbWeight.Size = new System.Drawing.Size(100, 23);
+            this.tbWeight.Size = new System.Drawing.Size(100, 36);
             this.tbWeight.TabIndex = 10;
             // 
             // tbCColors
             // 
-            this.tbCColors.Location = new System.Drawing.Point(109, 128);
+            this.tbCColors.Location = new System.Drawing.Point(166, 131);
             this.tbCColors.Name = "tbCColors";
-            this.tbCColors.Size = new System.Drawing.Size(100, 23);
+            this.tbCColors.Size = new System.Drawing.Size(100, 36);
             this.tbCColors.TabIndex = 9;
             // 
             // tbModel
             // 
-            this.tbModel.Location = new System.Drawing.Point(109, 98);
+            this.tbModel.Location = new System.Drawing.Point(166, 101);
             this.tbModel.Name = "tbModel";
-            this.tbModel.Size = new System.Drawing.Size(100, 23);
+            this.tbModel.Size = new System.Drawing.Size(100, 36);
             this.tbModel.TabIndex = 8;
             // 
             // tbCompany
             // 
-            this.tbCompany.Location = new System.Drawing.Point(109, 68);
+            this.tbCompany.Location = new System.Drawing.Point(166, 67);
             this.tbCompany.Name = "tbCompany";
-            this.tbCompany.Size = new System.Drawing.Size(100, 23);
+            this.tbCompany.Size = new System.Drawing.Size(100, 36);
             this.tbCompany.TabIndex = 7;
             // 
             // tbName
             // 
-            this.tbName.Location = new System.Drawing.Point(109, 38);
+            this.tbName.Location = new System.Drawing.Point(166, 38);
             this.tbName.Name = "tbName";
-            this.tbName.Size = new System.Drawing.Size(100, 23);
+            this.tbName.Size = new System.Drawing.Size(100, 36);
             this.tbName.TabIndex = 6;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(70, 191);
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(105, 191);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(33, 15);
+            this.label6.Size = new System.Drawing.Size(55, 25);
             this.label6.TabIndex = 5;
             this.label6.Text = "Ціна";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(71, 161);
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(108, 163);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(32, 15);
+            this.label5.Size = new System.Drawing.Size(52, 25);
             this.label5.TabIndex = 4;
             this.label5.Text = "Вага";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(9, 131);
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(8, 131);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(94, 15);
+            this.label4.Size = new System.Drawing.Size(152, 25);
             this.label4.TabIndex = 3;
             this.label4.Text = "К-сть Кольорів";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(50, 101);
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(74, 101);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 15);
+            this.label3.Size = new System.Drawing.Size(86, 25);
             this.label3.TabIndex = 2;
             this.label3.Text = "Модель";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(42, 71);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(59, 67);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 15);
+            this.label2.Size = new System.Drawing.Size(101, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "Компанія";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(62, 41);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(93, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.Size = new System.Drawing.Size(67, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Назва";
             // 
             // btnOk
             // 
             this.btnOk.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnOk.Location = new System.Drawing.Point(142, 107);
+            this.btnOk.Location = new System.Drawing.Point(125, 107);
             this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(67, 54);
+            this.btnOk.Size = new System.Drawing.Size(102, 54);
             this.btnOk.TabIndex = 1;
-            this.btnOk.Text = "Ок";
+            this.btnOk.Text = "Добавити";
             this.btnOk.UseVisualStyleBackColor = true;
             this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
             // 
@@ -190,7 +190,7 @@
             this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnCancel.Location = new System.Drawing.Point(20, 107);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(100, 54);
+            this.btnCancel.Size = new System.Drawing.Size(99, 54);
             this.btnCancel.TabIndex = 2;
             this.btnCancel.Text = "Скасувати";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -202,10 +202,10 @@
             this.groupBox2.Controls.Add(this.btnCancel);
             this.groupBox2.Controls.Add(this.cbWin);
             this.groupBox2.Controls.Add(this.btnOk);
-            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.groupBox2.Location = new System.Drawing.Point(12, 247);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(290, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(222, 177);
+            this.groupBox2.Size = new System.Drawing.Size(245, 177);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Загальні дані";
@@ -213,10 +213,10 @@
             // cbMac
             // 
             this.cbMac.AutoSize = true;
-            this.cbMac.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cbMac.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.cbMac.Location = new System.Drawing.Point(20, 75);
             this.cbMac.Name = "cbMac";
-            this.cbMac.Size = new System.Drawing.Size(132, 19);
+            this.cbMac.Size = new System.Drawing.Size(204, 29);
             this.cbMac.TabIndex = 5;
             this.cbMac.Text = "Підтримка Mac OS";
             this.cbMac.UseVisualStyleBackColor = true;
@@ -224,10 +224,10 @@
             // cbWin
             // 
             this.cbWin.AutoSize = true;
-            this.cbWin.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cbWin.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.cbWin.Location = new System.Drawing.Point(20, 33);
             this.cbWin.Name = "cbWin";
-            this.cbWin.Size = new System.Drawing.Size(141, 19);
+            this.cbWin.Size = new System.Drawing.Size(220, 29);
             this.cbWin.TabIndex = 4;
             this.cbWin.Text = "Підтримка Windows";
             this.cbWin.UseVisualStyleBackColor = true;
@@ -236,7 +236,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(240, 431);
+            this.ClientSize = new System.Drawing.Size(544, 270);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
